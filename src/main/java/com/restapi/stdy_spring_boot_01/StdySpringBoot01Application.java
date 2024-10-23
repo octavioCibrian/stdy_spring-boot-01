1f29902e3d23b92d5985a0598a813c5e8162fb3d
Octavio Rafael Cibrián Osuna <octavio.cibrian@gmail.com>
2024-10-22 21:50:10 -0400

    First commit

---
+++ src/main/java/com/restapi/stdy_spring_boot_01/StdySpringBoot01Application.java
@@ -0,0 +1,13 @@
+package com.restapi.stdy_spring_boot_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdySpringBoot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdySpringBoot01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/stdy_spring_boot_01/StdySpringBoot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.stdy_spring_boot_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdySpringBoot01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
